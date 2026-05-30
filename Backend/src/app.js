3d9332b44d74202b99ceec89e6dc3e14b93503da
Rohan Pal <rohanpal9833@gmail.com>
2026-05-30 13:48:02 +0530

    made the Signup LOgic Working

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,7 @@ const authenticationRoutes = require("./routes/authentication.routes.js");
 
 
 const app = express();
+app.use(express.json());
 
 app.use(cors({
   origin: 'http://localhost:5173', // React app ka URL
--- Backend/src/controllers/Signup.controllers.js
+++ Backend/src/controllers/Signup.controllers.js
@@ -1,12 +1,34 @@
-
-
+const UserModel = require("../model/user.model")
+const bcrypt = require("bcrypt")
 
 
 async function Signup(req,res) {
-    
+    console.log("Signup route hit");
+    console.log(req.body)
     const {name,email,password} = req.body;
 
-        
+    if(!email||!password||!name){
+        return res.status(4044).json({message:"enter all the data"});
+    }
+
+    let userexists = await UserModel.findOne({email});
+
+    if(userexists){
+        return res.status(409).json({message:"User already Exists"})
+    }
+
+    const Hashedpassword = await bcrypt.hash(password,7)
+
+
+    const user = await UserModel.create({
+        name,
+        email,
+        password:Hashedpassword
+    })
+
+    return res.status(200).json({message:"user created "})
+
+    
 
 
 
